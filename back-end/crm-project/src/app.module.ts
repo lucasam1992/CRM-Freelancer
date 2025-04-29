f8fa7aea947765f648e1881f93ef68c494d4b30f
lucasam1992 <lucas.almeida.machado92@gmail.com>
2025-04-29 10:57:06 -0300

    config database - localhost

--- back-end/crm-project/src/app.module.ts
+++ back-end/crm-project/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule],
+  controllers: [PrismaService],
+  providers: [PrismaService],
 })
 export class AppModule {}
--- back-end/crm-project/src/main.ts
+++ back-end/crm-project/src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import * as helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('CRM API')
+    .setDescription('The CRM API description')
+    .setVersion('1.0')
+    .addTag('crm')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  /*
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", "'unsafe-inline'"],
+          styleSrc: ["'self'", "'unsafe-inline'"],
+          imgSrc: ["'self'", 'data:', 'https:'],
+        },
+      },
+    }),
+  );
+  */
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
